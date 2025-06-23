3cd180526ac718069f26cd3f4a63fc996b12f061
fahrilmi25 <fahrilmi25@gmail.com>
2025-06-23 22:34:15 +0800

    sinkronisasi database dengan sequelize

--- Backend/index.js
+++ Backend/index.js
@@ -6,16 +6,11 @@ import db from "./config/Database.js";
 import noteRoutes from "./routes/index.js";
 import initRelations from "./models/InitRelaton.js";
 import router from "./routes/index.js";
+
 dotenv.config();
 const app = express();
 
-try {
-  await db.authenticate();
-  console.log("Database Connected...");
-} catch (error) {
-  console.log(error);
-}
-
+//ubah dengan url deploy frontend nanti
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
 app.use(express.json());
@@ -23,6 +18,23 @@ app.use(router);
 app.use("/avatars", express.static("public/avatars"));
 app.use(noteRoutes);
 
-initRelations()
+initRelations();
 
-app.listen(5000, () => console.log("Server running at port 5000"));
+const startServer = async () => {
+  try {
+    await db.authenticate();
+    console.log("Database Connected...");
+
+    await db.sync();
+    console.log("All models were synchronized successfully.");
+
+    app.listen(5000, () => {
+      console.log("Server running at port 5000");
+    });
+    
+  } catch (error) {
+    console.error("Unable to start server:", error);
+  }
+};
+
+startServer();
